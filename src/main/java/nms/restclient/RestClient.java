01b920f56d802842e62a5c622e7dd460606b02fb
elmiomar <elmimouni.o.i@gmail.com>
2020-08-30 21:44:39 -0400

    add login function

--- src/main/java/nms/restclient/RestClient.java
+++ src/main/java/nms/restclient/RestClient.java
@@ -9,5 +9,7 @@ public interface RestClient {
 	public Future<Configuration> getRunningConfiguration();
 	
 	public Future<Void> sendNotification(Notification notification);
+	
+	public Future<String> basicAuthentication(String user, String password);
 
 }
--- src/main/java/nms/restclient/RestClientImpl.java
+++ src/main/java/nms/restclient/RestClientImpl.java
@@ -6,34 +6,40 @@ import io.vertx.core.Future;
 import io.vertx.core.Promise;
 import io.vertx.core.Vertx;
 import io.vertx.core.buffer.Buffer;
+import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.client.HttpResponse;
 import io.vertx.ext.web.client.WebClient;
+import io.vertx.ext.web.codec.BodyCodec;
 
 public class RestClientImpl implements RestClient {
 
 	private EntryPoint entryPoint;
 	private WebClient webClient;
-	
+
 	private static String CANDIDATE_CONFIG_ENDPOINT = "/configuration/candidate-config";
 	private static String RUNNING_CONFIG_ENDPOINT = "/configuration/running-config";
 	private static String NOTIFICATION_STATUS_ENDPOINT = "/notification/status";
 	private static String NOTIFICATION_EVENT_ENDPOINT = "/notification/event";
 	private static String NOTIFICATION_FAULT_ENDPOINT = "/notification/fault";
+	private static String LOGIN_ENDPOINT = "/login/agent";
 	
+	private String token;
+
 	public RestClientImpl(Vertx vertx, final EntryPoint entryPoint) {
 		this.webClient = WebClient.create(vertx);
 		this.entryPoint = entryPoint;
 	}
-	
+
 	public RestClientImpl(EntryPoint entryPoint) {
 		this.entryPoint = entryPoint;
 	}
-	
-	
+
 	@Override
 	public Future<Configuration> getCandidateConfiguration() {
 		Promise<Configuration> promise = Promise.promise();
-		webClient.get(entryPoint.getPort(), entryPoint.getHost(), CANDIDATE_CONFIG_ENDPOINT).send(ar -> {
+		webClient
+		.get(entryPoint.getPort(), entryPoint.getHost(), CANDIDATE_CONFIG_ENDPOINT)
+		.send(ar -> {
 			if (ar.succeeded()) {
 				HttpResponse<Buffer> response = ar.result();
 				Configuration config = new Gson().fromJson(response.bodyAsString(), Configuration.class);
@@ -48,7 +54,9 @@ public class RestClientImpl implements RestClient {
 	@Override
 	public Future<Configuration> getRunningConfiguration() {
 		Promise<Configuration> promise = Promise.promise();
-		webClient.get(entryPoint.getPort(), entryPoint.getHost(), RUNNING_CONFIG_ENDPOINT).send(ar -> {
+		webClient
+		.get(entryPoint.getPort(), entryPoint.getHost(), RUNNING_CONFIG_ENDPOINT)
+		.send(ar -> {
 			if (ar.succeeded()) {
 				HttpResponse<Buffer> response = ar.result();
 				Configuration config = new Gson().fromJson(response.bodyAsString(), Configuration.class);
@@ -63,8 +71,58 @@ public class RestClientImpl implements RestClient {
 	@Override
 	public Future<Void> sendNotification(Notification notification) {
 		Promise<Void> promise = Promise.promise();
+		String endpoint = "";
+		switch (notification.getType()) {
+		case STATUS:
+			endpoint = NOTIFICATION_STATUS_ENDPOINT;
+			break;
+		case EVENT:
+			endpoint = NOTIFICATION_EVENT_ENDPOINT;
+			break;
+		case FAULT:
+			endpoint = NOTIFICATION_FAULT_ENDPOINT;
+			break;
+		}
+
 		// TODO: add logic to send notification here
+		// this should be a post request
+		// look at: https://vertx.io/docs/vertx-web-client/java/#_writing_request_bodies
 		return promise.future();
+	}
+
+	@Override
+	public Future<String> basicAuthentication(String user, String password) {
+		Promise<String> promise = Promise.promise();
+		webClient
+		.post(entryPoint.getPort(), entryPoint.getHost(), LOGIN_ENDPOINT)
+		.as(BodyCodec.jsonObject()) // response will be decoded as JsonObject
+		.sendJsonObject(new JsonObject()
+				.put("username", user)
+				.put("password", password), ar -> {
+					if (ar.succeeded()) {
+						HttpResponse<JsonObject> response = ar.result();
+						String token = response.body().getString("token");
+						this.setToken(token);
+						promise.complete(token);
+					} else {
+						promise.fail("unable to login");
+					}
+				});
+		return promise.future();
+	}
+
+	/**
+	 * @return the token
+	 */
+	public String getToken() {
+		return token;
+	}
+
+	/**
+	 * @param token the token to set
+	 */
+	public void setToken(String token) {
+		this.token = token;
 	}
 
 }
