8391331be468ea0932134fd4c4bca1d31ff43973
elmiomar <elmimouni.o.i@gmail.com>
2020-09-22 16:40:24 -0400

    update verticle

--- src/main/java/nms/restclient/WebClientVerticle.java
+++ src/main/java/nms/restclient/WebClientVerticle.java
@@ -1,9 +1,5 @@
 package nms.restclient;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.Properties;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.AsyncResult;
@@ -13,53 +9,83 @@ import io.vertx.core.Promise;
 import io.vertx.core.json.JsonObject;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
-import io.vertx.ext.web.client.WebClient;
-import nms.restclient.service.AuthenticationService;
-import nms.restclient.service.ConfigurationService;
-import nms.restclient.service.NotificationService;
-import nms.restclient.service.TokenManager;
-import nms.restclient.service.impl.ConfigurationServiceImpl;
-import nms.restclient.service.impl.Login;
-import nms.restclient.service.impl.NotificationServiceImpl;
-import nms.restclient.service.impl.TokenManagerImpl;
+import nms.restclient.service.impl.CredentialsProvider;
 
 public class WebClientVerticle extends AbstractVerticle {
 
-	private WebClient webClient;
-	
-	private TokenManager tokenManager;
-	private ConfigurationService configService;
-	private NotificationService notifService;
+	private RestClient restClient;
 
 	private static final Logger LOG = LoggerFactory.getLogger(WebClientVerticle.class);
-	private static String WEBCLIENT_EVENTBUS_ADDRESS = "webclient-verticle.eventbus";
+//	private static String WEBCLIENT_EVENTBUS_ADDRESS = "webclient-verticle.eventbus";
 	private static String FORWARDER_EVENTBUS_ADDRESS = "fw-verticle.eventbus";
 	private static String RIB_EVENTBUS_ADDRESS = "rib-verticle.eventbus";
-	boolean isNewConfig = true;
-	
-	private AuthToken authToken;
+	private static long CONFIG_PERIOD = 150000; // delay the delay in milliseconds, after which the timer will fire
+	private static long STATUS_PERIOD = 100000; // 100s
+
+	private static Handler<Long> statusTask;
+	private static Handler<Long> configTask;
 
 	@Override
 	public void start(Promise<Void> promise) throws Exception {
 		super.start(promise);
 		LOG.info("starting " + this.getClass().getName());
-		this.webClient = WebClient.create(vertx);
-		EntryPoint localEntryPoint = new EntryPoint(9001, "localhost");
-//		TokenManagerImpl(webClient, localEntryPoint);
-		this.login().onComplete(ar -> {
+		this.restClient = new RestClientImpl(vertx);
+		CredentialsProvider provider = new CredentialsProvider("data.properties");
+		this.login(provider.getUsername(), provider.getPassword()).onComplete(ar -> {
 			if (ar.succeeded()) {
-				authToken = ar.result();
-				this.notifService = new NotificationServiceImpl(webClient, localEntryPoint, authToken);
-				this.configService = new ConfigurationServiceImpl(webClient, localEntryPoint);
+				LOG.info("agent connected succesfully!");
+				String token = ar.result();
+				LOG.info("token=" + token);
+				this.restClient.setToken(token);
+
+				this.sendStatus(ar1 -> {
+					if (ar1.succeeded()) {
+						LOG.info("sent status to controller");
+					} else {
+						LOG.info("cannot send status");
+					}
+				});
+				
+
+//				
+//				this.pollConfiguration(new Handler<AsyncResult<Configuration>>() {
+//					@Override
+//					public void handle(AsyncResult<Configuration> ar) {
+//						if (ar.succeeded()) {
+//							LOG.info("got new configuration from the controller");
+//							LOG.info(ar.result().toJsonObject());
+//						} else {
+//							LOG.info("unable to retrive the configuration: " + ar.result());
+//						}
+//					}
+//				});
+			} else {
+				LOG.info("unable to login, reason: " + ar.cause());
 			}
 		});
 	}
 
+	private Future<String> login(String username, String password) {
+		return this.restClient.login(username, password);
 
-	private Future<AuthToken> login() {
-		return this.tokenManager.getNewToken();
 	}
 
+
+	private void sendStatus(Handler<AsyncResult<Void>> handler) {
+		statusTask = id -> {
+			this.restClient.sendStatus().onComplete(handler);
+			vertx.setTimer(STATUS_PERIOD, statusTask); // send every 100s
+		};
+		vertx.setTimer(1000, statusTask); // send first status after 1 second
+	}
+
+	private void pollConfiguration(Handler<AsyncResult<Configuration>> handler) {
+		configTask =  id -> {
+			this.restClient.getConfiguration().onComplete(handler);
+			vertx.setTimer(CONFIG_PERIOD, configTask); // refresh config every 150s
+		};
+		vertx.setTimer(1000, configTask ); // get initial config after 1 second
+	}
 
 	private void applyConfiguration(Configuration config) {
 		config.getFaces().forEach(face -> {
