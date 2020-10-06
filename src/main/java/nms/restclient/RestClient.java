de5990f608fd7e2fc7542b2dea61a396d86ff074
elmiomar <elmimouni.o.i@gmail.com>
2020-10-06 13:40:33 -0400

    update interface

--- src/main/java/nms/restclient/RestClient.java
+++ src/main/java/nms/restclient/RestClient.java
@@ -1,18 +1,21 @@
 package nms.restclient;
 
-import io.vertx.core.AsyncResult;
 import io.vertx.core.Future;
-import io.vertx.core.Handler;
 
 public interface RestClient {
 
 	public Future<Configuration> getConfiguration();
-
 
 	public Future<String> login(String user, String password);
 
 	public void setToken(String token);
 
 	public Future<Void> sendStatus();
+
+	public void setRootCA(String path);
+
+	public void setPort(Integer port);
+
+	public void setHost(String host);
 
 }
